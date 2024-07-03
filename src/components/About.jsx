763c0f80bee0d9fc41620195f70fdea8d25e2d47
linnjoha <linn.wallin@hotmail.com>
2024-07-03 19:37:41 +0200

    rootheight

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -18,35 +18,39 @@ const AboutCard = ({ title, icon }) => {
 const About = () => {
   return (
     <>
-      <div className="about-wrapper">
-        <div className="about-header">
-          <p className="hero-sub-text ">Introduction</p>
-          <h2 className="hero-text">about.</h2>
-          <motion.p className="about-text">
-            I have a passion for creating and developing with a great eye for
-            details and design. My expertise is in JavaScript, TypeScript,
-            React, Node.js together with database and cloud development. <br />
-            If you don't find me nerding by the computer you'll find me spending
-            time with my family, gardening or chasing a good workout outside.
-          </motion.p>
-          <Socials />
+      <div>
+        <div className="about-wrapper">
+          <div className="about-header">
+            <p className="hero-sub-text ">Introduction</p>
+            <h2 className="hero-text">about.</h2>
+            <motion.p className="about-text">
+              I have a passion for creating and developing with a great eye for
+              details and design. My expertise is in JavaScript, TypeScript,
+              React, Node.js together with database and cloud development.{" "}
+              <br />
+              If you don't find me nerding by the computer you'll find me
+              spending time with my family, gardening or chasing a good workout
+              outside.
+            </motion.p>
+            <Socials />
+          </div>
+          <motion.div
+            initial={{ opacity: 0, scale: 0.5 }}
+            animate={{ opacity: 1, scale: 1 }}
+            transition={{ ease: "easeOut", duration: 1 }}
+            className="profileImage-container"
+          >
+            <img src={profile} alt="profileImage" className="profile-img" />
+          </motion.div>
         </div>
-        <motion.div
-          initial={{ opacity: 0, scale: 0.5 }}
-          animate={{ opacity: 1, scale: 1 }}
-          transition={{ ease: "easeOut", duration: 1 }}
-          className="profileImage-container"
-        >
-          <img src={profile} alt="profileImage" className="profile-img" />
-        </motion.div>
+        {
+          <div className="cards-container">
+            {abouts.map((about, index) => (
+              <AboutCard key={about.title} index={index} {...about} />
+            ))}
+          </div>
+        }
       </div>
-      {
-        <div className="cards-container">
-          {abouts.map((about, index) => (
-            <AboutCard key={about.title} index={index} {...about} />
-          ))}
-        </div>
-      }
     </>
   );
 };
